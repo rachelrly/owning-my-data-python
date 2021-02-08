1b2e79878174f921ce583266c9abad968cdc87ad
Rachel <r.a.reill18@gmail.com>
2021-02-08 12:02:30 -0600

    added func to normalize title before inserting in dict

--- spotify.py
+++ spotify.py
@@ -1,23 +1,49 @@
 import json
 import datetime
+import re
+#import matplotlib.pyplot as plt
+
+def normaize_title(str):
+    formatted = ''
+    for l in str:
+        if re.match(r'\w', l):
+            print('past if statement')
+            formatted += l.lower()
+        else: 
+            formatted += '_'
+    return formatted
+
 
 def print_spotify_activity():
     #open file
     sh0 = open('StreamingHistory0.json', )
     sh1 = open('StreamingHistory1.json', )
     #convert to python
-    data0 = json.load(sh0)
-    data1 = json.load(sh1)
+    data = json.load(sh0) + json.load(sh1)
 
-    for i in data0:
+    songs = dict()
+
+    for i in data:
        # ts = datetime.datetime.fromtimestamp(i['endTime'])
-        a = i.get('artistName')
-        c = i.get('trackName')
         s = i.get('msPlayed')
-        print(f'{a} from {c}, {s}')
+        if s > 10000:
+            t = i.get('endTime')
+            a = i.get('artistName')
+            c = i.get('trackName')
+            print(c)
+            if c in songs:
+                print(songs[c])
+                songs[c] = songs[c] + 1
+            else:
+                #format songs for dict
+                songs[c] = 1
+                # create a sub-dictionary with 
 
     sh0.close()
     sh1.close()
+    # for s in songs:
+    #     if s > 5:
+           # print(s)
 
 
-print_spotify_activity()
+#print_spotify_activity()
